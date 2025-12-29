1f19d3e182de85febacaf573c08b1469f67b639a
Anandhbasha <anadhakm@gmail.com>
2025-12-29 11:06:37 +0530

    basics

---
+++ components/src/App.jsx
@@ -0,0 +1,14 @@
+import Button from "./Button"
+
+function App(){
+  return (
+    <>
+      <Button value="Click" />
+      <Button value="Login"/>
+      <Button value="Edit"/>
+      <Button value="Delete"/>
+    </>
+  )
+}
+
+export default App
